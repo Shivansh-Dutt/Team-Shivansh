4439864f90f0818d28f6fffa342759e6fe39ee36
Shivansh_dutt <shivanshs497@gmail.com>
2025-01-03 22:56:00 +0530

    connect server with database

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,18 @@
 import express from "express"
+import userRoutes from "./routes/user.route.js";
+import ConnectDB from "./utils/db.js";
 
-const app = express()
+const app = express();
 
+// Middleware
+app.use(express.json());
+
+// Connect to MongoDB
+ConnectDB();
+
+// Middleware for routes
+app.use("/api/v1/user", userRoutes);
+
+// Port and Server
 const PORT = 8000
-
-app.listen(PORT,()=>{
-    console.log(`Server is listening at port ${PORT}`)
-})
+app.listen(PORT, () => console.log(`Server listening on port ${PORT}`));
