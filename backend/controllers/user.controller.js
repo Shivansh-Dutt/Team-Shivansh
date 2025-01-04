6b6aa65192ce45ed7ffc18600e5318cfa9aaac8a
Shivansh_dutt <shivanshs497@gmail.com>
2025-01-04 11:34:17 +0530

    Add middleware for authentication

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -1,14 +1,14 @@
 import User from "../models/user.model.js";
 import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
-import dotenv from "dotenv"
+import dotenv from "dotenv";
 
 export const register = async (req, res) => {
   const { name, email, password, role, skills, location } = req.body;
 
   try {
-    if(!name || !email || !password || !role){
-        return res.status(401).json({message : "Fill all data"})
+    if (!name || !email || !password || !role) {
+      return res.status(401).json({ message: "Fill all data" });
     }
 
     // Check if user already exists
@@ -33,89 +33,105 @@ export const register = async (req, res) => {
     await user.save();
     res.status(201).json({ message: "User registered successfully", user });
   } catch (error) {
-    res.status(500).json({ message: "Error registering user", error: error.message });
+    res
+      .status(500)
+      .json({ message: "Error registering user", error: error.message });
   }
 };
 
-
-
 // function to login a user
 
-export const login = async(req,res)=>{
-    try {
-        const {email,password} = req.body
-        // check both data are given
-        if(!email || !password){ 
-            return res.status(401).json({
-                message : "Invalid Data",
-                success:true
-            })
-        }
-
-        // check for user in db
-        let user = await User.findOne({email})
-        if(!user){
-            return res.status(401).json({
-                message:"Invalid Credentials",
-                success: false
-            });
-        }
-        // check password is matching
-        const isPasswordMatch = await bcrypt.compare(password,user.password)
-        if(!isPasswordMatch){
-            return res.status(401).json({
-                message:"Invalid Credentials",
-                success: false
-            });
-        }
-        
-        
-        // create a token
-        const token = await jwt.sign({userID:user._id},process.env.SECRET_KEY,{expiresIn:'1d'})
-        
-        user = {
-            _id:user._id,
-            username:user.username,
-            email:user.email,
-            profilePicture:user.profilePicture,
-            bio:user.bio,
-            followers:user.followers,
-            following:user.following,
-            posts:user.posts,
-            gender:user.gender
-        }
-        return res.cookie('token',token,{httpOnly:true,sameSite:'strict',maxAge:1*24*60*60*1000}).json({
-            message:`Welcome back ${user.username}`,
-            success:true,
-            user
-        })
-
-    } catch (error) {
-        console.log(error)
-        return res.status(404).json({
-            message:"Server Error",
-            success:false
-        })
+export const login = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    // check both data are given
+    if (!email || !password) {
+      return res.status(401).json({
+        message: "Invalid Data",
+        success: true,
+      });
     }
-}
+
+    dotenv.config();
+
+    // check for user in db
+    let user = await User.findOne({ email });
+    if (!user) {
+      return res.status(401).json({
+        message: "Invalid Credentials",
+        success: false,
+      });
+    }
+    // check password is matching
+    const isPasswordMatch = await bcrypt.compare(password, user.password);
+    if (!isPasswordMatch) {
+      return res.status(401).json({
+        message: "Invalid Credentials",
+        success: false,
+      });
+    }
+
+    // create a token
+    const token = await jwt.sign({ userID: user._id }, process.env.SECRET_KEY, {
+      expiresIn: "1d",
+    });
+
+    return res
+      .cookie("token", token, {
+        httpOnly: true,
+        sameSite: "strict",
+        maxAge: 1 * 24 * 60 * 60 * 1000,
+      })
+      .json({
+        message: `Welcome back ${user.name}`,
+        success: true,
+        user,
+      });
+  } catch (error) {
+    console.log(error);
+    return res.status(404).json({
+      message: "Server Error",
+      success: false,
+    });
+  }
+};
+
+export const getProfile = async (req, res) => {
+  try {
+
+    const user = await User.findById(req.id).select("-password");
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
+    }
+
+    res.status(200).json(user);
+  } catch (error) {
+    console.log(error)
+    res
+      .status(500)
+      .json({ message: "Error fetching profile", error: error.message });
+  }
+};
 
 export const updateProfile = async (req, res) => {
-    const { name, location, skills } = req.body;
-  
-    try {
-      const user = await User.findById(req.user.id);
-      if (!user) {
-        return res.status(404).json({ message: "User not found" });
-      }
-  
-      user.name = name || user.name;
-      user.location = location || user.location;
-      user.skills = skills || user.skills;
-  
-      await user.save();
-      res.status(200).json({ message: "Profile updated successfully", user });
-    } catch (error) {
-      res.status(500).json({ message: "Error updating profile", error: error.message });
+  const { name, location, skills } = req.body;
+
+  try {
+    const user = await User.findById(req.id);
+
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
     }
-  };
-  
+
+    user.name = name || user.name;
+    user.location = location || user.location;
+    user.skills = skills || user.skills;
+
+    await user.save();
+    res.status(200).json({ message: "Profile updated successfully", user });
+  } catch (error) {
+    res
+      .status(500)
+      .json({ message: "Error updating profile", error: error.message });
+  }
+};
--- backend/routes/user.route.js
+++ backend/routes/user.route.js
@@ -1,10 +1,12 @@
 import express from "express";
-import { register ,login, updateProfile} from "../controllers/user.controller.js";
+import { register ,login, updateProfile, getProfile} from "../controllers/user.controller.js";
+import isAuthentiacted from "../middlewares/isAuthenticated.js";
 
 const router = express.Router()
 
 router.post("/register", register);
 router.post("/login", login);
-router.put("/updateProfile",updateProfile)
+router.get("/:id/getProfile",isAuthentiacted,getProfile)
+router.post("/profile/update",isAuthentiacted,updateProfile)
 
 export default router;
